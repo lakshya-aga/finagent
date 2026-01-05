a7219b440e681dc985c83c55f16268cb8c3956c3
lakshya-aga <lakshya001@ntu.edu.sg>
2026-01-05 22:31:54 +0530

    Added read file content tool

---
+++ functions/get_file_content.py
@@ -0,0 +1,18 @@
+import os
+
+def get_file_content(working_directory, file_path="."):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_file = os.path.normpath(os.path.join(abs_working_dir, file_path))
+
+    if os.path.commonpath([target_file, abs_working_dir]) != abs_working_dir:
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        
+    if not os.path.isfile(target_file):
+        print(target_file)
+        return f'Error: "{file_path}" is not a file'
+    MAX_CHARS = 10000
+    with open(target_file, 'r') as f:
+        ans = f.read(MAX_CHARS)
+        if(f.read(1)):
+            ans += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+    return ans
