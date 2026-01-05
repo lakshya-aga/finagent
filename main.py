3cffacaee345654d985b6a703c9d1bb2992c1698
lakshya-aga <lakshya001@ntu.edu.sg>
2026-01-05 20:56:36 +0530

    chapter 5

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-if api_key is None:
-    raise RuntimeError("No Key found")
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("No Key found")
+    client = genai.Client(api_key=api_key)
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
 
-response = client.models.generate_content(
-    model='gemini-2.5-flash', contents = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', contents = args.user_prompt
+    )
+    if response.usage_metadata is None:
+        print("Response is None")
+    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    print(response.text)
 
-print(response.text)
+main()
